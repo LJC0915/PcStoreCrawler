f5120fc996b63e89a6bd0b9a5ea774fd5bdc7d7f
Errol <errol@Errols-iMac.local>
2019-04-10 21:52:34 +0800

    impl: crawler

---
+++ Crawler.py
@@ -0,0 +1,18 @@
+import base64
+import requests
+from bs4 import BeautifulSoup
+from urllib import parse
+
+
+inputText = input("請輸入關鍵字: ")
+searchText = base64.b64encode(str.encode(parse.quote(inputText))).decode()
+url = "https://www.pcstore.com.tw/adm/psearch.htm?store_k_word=" + \
+    searchText + "&slt_k_option=1"
+
+res = requests.get(url)
+res.encoding = 'big5'
+soup = BeautifulSoup(res.text, 'html.parser')
+item = soup.select("div.pic2t_bg > a")
+titleArray = []
+for i in item:
+    titleArray.append(i.get_text())
